cb5203aa8a4a7c662f8c5b92ca1e7963297e88b8
Jim DeVona <jim.devona@gmail.com>
2012-03-22 00:21:34 -0400

    Implemented hack to optionally --split line segments that cross the 180th mer. for compatibility with fragile GIS (most, it appears). Split segments are output as a single multipart feature. The split longitude is +/-180 and the latitude intercept is naively interpolated. Determining which of the 180th or prime mers is crossed is accomplished with a placeholder proximity test and needs to be improved.

--- Source/gtgshp.cpp
+++ Source/gtgshp.cpp
@@ -40,34 +40,20 @@ ShapefileWriter::ShapefileWriter(const char *basepath, enum output_format_type f
 	}
 }
 
-// assumes we're dealing with the ±180 meridian, not the 0 meridian
-// determining which is which is a tricky issue.
-// best idea for a solution so far is to choose whichever meridian
-// is closer to the longitude; this implies some arbitrary limit on interval
-// size - steps which span a half hemisphere or more would trick that method
-
-
-double DistanceToMeridian(double lon, bool prime)
+double DistanceToMeridian(double lon)
 {
 	double result;
 	
-	if (prime) {
-		result = -1.0 * lon;
+	// for prime meridian:
+	// result = -1.0 * lon;
+	
+	if (lon < 0) {
+		result = -180 - lon;
 	} else {
-		if (lon < 0) {
-			result = -180 - lon;
-		} else {
-			result = 180 - lon;
-		}
-	}	
+		result = 180 - lon;
+	}
 	
 	return result;
-}
-
-
-double AbsoluteDistanceToMeridian(double lon, bool prime)
-{
-	return fabs(DistanceToMeridian(lon, prime));
 }
 
 int ShapefileWriter::output(Eci *loc, Eci *nextloc)
@@ -75,7 +61,7 @@ int ShapefileWriter::output(Eci *loc, Eci *nextloc)
 	CoordGeodetic locg(loc->ToGeodetic());
 	double latitude[2];
 	double longitude[2];
-	SHPObject *obj;
+	SHPObject *obj = NULL;
 	int index;
 	int pointc = 1;
 	
@@ -94,37 +80,74 @@ int ShapefileWriter::output(Eci *loc, Eci *nextloc)
 		pointc = 2;
 		latitude[1] = Util::RadiansToDegrees(nextlocg.latitude);
 		longitude[1] = Util::RadiansToDegrees(nextlocg.longitude);
-		
+
+		/* This line segment's endpoints are in different E/W hemispheres;
+	    */		
 		if (cfg.split && ((longitude[0] > 0 && longitude[1] < 0) || (longitude[0] < 0 && longitude[1] > 0))) {
-			// crosses a meridian.
-			// we really only care about the prime antimeridian (±180˚ longitude)
 
-			double x1 = longitude[0];
-			double y1 = latitude[0];
-			double x2 = longitude[1];
-			double y2 = latitude[1];
-
-			printf("meridian crossing detected\n");
+			/* FUDGEWORK. We assume this line segment crosses the 180th
+			   meridian instead of the prime meridian simply because both
+			   endpoint longitude values are closer to 180 than 0. This may
+			   be incorrect in a variety of cases. For example, large model
+			   intervals may result in such widely spaced points that the
+			   trace skips from hemisphere to hemisphere (or orbit to orbit).
+			   
+			   Practically speaking, --format line is intended to produce
+			   smooth-looking track lines, so OK small intervals are typical.
+			   (Close to the poles, the absolute size of even small intervals
+			   may place endpoints such that this is confused. In these cases,
+			   even if it is correctly determined that the track crosses the
+			   180th meridian, the cartesian intercept interpolation may split
+			   the intercept in the wrong place. Great circles, man!)
+			   
+			   We could forget about trying to ID which meridian, and just
+			   output split segments for the 180th AND prime meridians.
+			   However, limitations of this interpolation technique still
+			   apply - and it requires the distance to the crossed meridian,
+			   meaning the problem of identifying which that is is not resolved.
+			   
+			   (Looking at polar test cases are helpful.)
+			   
+			   Bottom line, this |longitudes| > 90 test is bogus.
+			   
+			   */
+			if (fabs(longitude[0]) > 90 && fabs(longitude[1]) > 90) {
+				
+				/* naive interpolation of meridian intercept */
+				double x1 = longitude[0];
+				double y1 = latitude[0];
+				double x2 = longitude[1];
+				double y2 = latitude[1];						
+				double d1 = fabs(DistanceToMeridian(x1));
+				double d2 = fabs(DistanceToMeridian(x2));
+				double dy = y2 - y1;
+				double dx = d1 + d2;
+				double m  = dy / dx;
+				double yi = (m * DistanceToMeridian(x1)) + y1;
+				
+				/* all vertices of multipart segments go in the same xy arrays;
+				   a separate "part start" array points to the start of each */
+				double xv[4];
+				double yv[4];
+				int partindices[2];
+				
+				partindices[0] = 0;
+				xv[0] = x1;
+				yv[0] = y1;			
+				xv[1] = x1 < 0 ? -180 : 180;
+				yv[1] = yi;
+				
+				partindices[1] = 2;
+				xv[2] = x1 < 0 ? 180 : -180;
+				yv[2] = yi;
+				xv[3] = x2;
+				yv[3] = y2;
 	
-			bool prime;
-			if (fabs(x1) < 90 && fabs(x2) < 90) {
-				prime = true;
-				printf("my guess is that this is the prime meridian\n");
-			} else {
-				prime = false;
-				printf("my guess is that this is the 180th meridian\n");
+				Note("180th meridian latitude intercept: %lf\n", yi);
+				if (NULL == (obj = SHPCreateObject(shpFormat_, -1, 2, partindices, NULL, 4, xv, yv, NULL, NULL))) {
+					Fail("cannot create split line segment\n");
+				}
 			}
-						
-			double d1 = AbsoluteDistanceToMeridian(x1, prime);
-			double d2 = AbsoluteDistanceToMeridian(x2, prime);
-			double dy = y2 - y1;
-			double dx = d1 + d2;
-			double m  = dy / dx;
-			double ym = m * DistanceToMeridian(x1, prime) + y1;
-			
-			printf("suggested latitude intercept ym = %lf\n", ym);
-			
-						
 		}
 			
 	} else if (shpFormat_ == SHPT_ARC) {
@@ -132,7 +155,11 @@ int ShapefileWriter::output(Eci *loc, Eci *nextloc)
 	}
 		
 	/* output the geometry */
-	obj = SHPCreateSimpleObject(shpFormat_, pointc, longitude, latitude, NULL);
+	if (NULL == obj) {
+		// in most cases we'll need to do this, but if we crossed a key meridian
+		// we'll already have a split shape object to output
+		obj = SHPCreateSimpleObject(shpFormat_, pointc, longitude, latitude, NULL);
+	}
 	if (NULL == obj) {
 		Fail("cannot create shape\n"); // not very informative
 	}
