3c88922f90b494bc95d37c553c16b8382a1cd259
Jim DeVona <jim.devona@gmail.com>
2012-03-21 22:40:29 -0400

    Working on an optional hack to split line segments that cross 180 lon. Here is rough draft code to guess the situation and naively interpolate the latitude of the split point.

--- Source/gtgshp.cpp
+++ Source/gtgshp.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #include "gtgshp.h"
 #include "gtgutil.h"
@@ -39,6 +40,36 @@ ShapefileWriter::ShapefileWriter(const char *basepath, enum output_format_type f
 	}
 }
 
+// assumes we're dealing with the ±180 meridian, not the 0 meridian
+// determining which is which is a tricky issue.
+// best idea for a solution so far is to choose whichever meridian
+// is closer to the longitude; this implies some arbitrary limit on interval
+// size - steps which span a half hemisphere or more would trick that method
+
+
+double DistanceToMeridian(double lon, bool prime)
+{
+	double result;
+	
+	if (prime) {
+		result = -1.0 * lon;
+	} else {
+		if (lon < 0) {
+			result = -180 - lon;
+		} else {
+			result = 180 - lon;
+		}
+	}	
+	
+	return result;
+}
+
+
+double AbsoluteDistanceToMeridian(double lon, bool prime)
+{
+	return fabs(DistanceToMeridian(lon, prime));
+}
+
 int ShapefileWriter::output(Eci *loc, Eci *nextloc)
 {
 	CoordGeodetic locg(loc->ToGeodetic());
@@ -62,7 +93,40 @@ int ShapefileWriter::output(Eci *loc, Eci *nextloc)
 		CoordGeodetic nextlocg(nextloc->ToGeodetic());
 		pointc = 2;
 		latitude[1] = Util::RadiansToDegrees(nextlocg.latitude);
-		longitude[1] = Util::RadiansToDegrees(nextlocg.longitude);		
+		longitude[1] = Util::RadiansToDegrees(nextlocg.longitude);
+		
+		if ((longitude[0] > 0 && longitude[1] < 0) || (longitude[0] < 0 && longitude[1] > 0)) {
+			// crosses a meridian.
+			// we really only care about the prime antimeridian (±180˚ longitude)
+
+			double x1 = longitude[0];
+			double y1 = latitude[0];
+			double x2 = longitude[1];
+			double y2 = latitude[1];
+
+			printf("meridian crossing detected\n");
+	
+			bool prime;
+			if (fabs(x1) < 90 && fabs(x2) < 90) {
+				prime = true;
+				printf("my guess is that this is the prime meridian\n");
+			} else {
+				prime = false;
+				printf("my guess is that this is the 180th meridian\n");
+			}
+						
+			double d1 = AbsoluteDistanceToMeridian(x1, prime);
+			double d2 = AbsoluteDistanceToMeridian(x2, prime);
+			double dy = y2 - y1;
+			double dx = d1 + d2;
+			double m  = dy / dx;
+			double ym = m * DistanceToMeridian(x1, prime) + y1;
+			
+			printf("suggested latitude intercept ym = %lf\n", ym);
+			
+						
+		}
+			
 	} else if (shpFormat_ == SHPT_ARC) {
 		Fail("line output requires two points; only one received\n");
 	}
